e66000c92dab47c9673132588b1007d90b4a6dd7
WXY-Belief <2466764951@qq.com>
2023-07-13 17:16:44 +0800

    update

---
+++ haha/Nucleus_Recongnition_Cut_img.py
@@ -0,0 +1,169 @@
+import os
+import cv2
+import mmcv
+import copy
+import time
+import numpy as np
+import pandas as pd
+from scipy import ndimage
+from .deep_learing_model.mmseg.apis import inference_segmentor, init_segmentor
+from .deep_learing_model.mmseg.datasets import result_to_inst
+from .deep_learing_model.tools.test import update_legacy_cfg
+from skimage.segmentation import find_boundaries
+from PIL import Image
+
+Image.MAX_IMAGE_PIXELS = None
+cut_size = 512  # size of cut img
+config = "./haha/deep_learing_model/configs/config_cut_img.py"
+checkpoint = "./haha/deep_learing_model/final_model/iter_56000.pth"
+
+
+def make_dir(path, dir_name) -> str:
+    os.makedirs(os.path.join(path, dir_name), exist_ok=True)
+    return os.path.join(path, dir_name)
+
+
+def find_dapi_img(path):
+    for file_name in os.listdir(path):
+        if os.path.join(path, file_name).lower().endswith(('.png', '.jpg', '.tif')):
+            return os.path.join(path, file_name)
+
+
+def cut_img(dapi_path, save_path) -> None:
+    global row_size, col_size
+    ori_img = Image.open(dapi_path).convert("RGB")
+    row_size, col_size = ori_img.height, ori_img.width
+
+    ori_img_512 = np.zeros((row_size + cut_size, col_size + cut_size, 3))
+    ori_img_512[0:row_size, 0:col_size, :] = ori_img
+
+    num_row, num_col = int(ori_img_512.shape[0] / cut_size), int(ori_img_512.shape[1] / cut_size)
+    for i in range(0, num_row):
+        for j in range(0, num_col):
+            final_cut_img = ori_img_512[i * cut_size: (i + 1) * cut_size, j * cut_size: (j + 1) * cut_size, :]
+            cv2.imwrite(os.path.join(save_path, '{}_{}.PNG'.format(i + 1, j + 1)), final_cut_img)
+
+
+# Draw the outline of nucleus
+def make_outline_overlay(RGB_data, predictions):
+    boundaries = np.zeros_like(predictions)
+    overlay_data = copy.copy(RGB_data)
+
+    for img in range(predictions.shape[0]):
+        boundary = find_boundaries(predictions[img, :, :], connectivity=1, mode='inner')
+        boundaries[img, boundary > 0] = 1
+
+    overlay_data[boundaries > 0, :] = (0, 255, 0)
+
+    return overlay_data
+
+
+def predict(base_dir, img_path, prediction_result_path, boundary_result_path, device, threshold=0.5):
+    infos = pd.DataFrame()
+    boundery_img = np.zeros((row_size + cut_size, col_size + cut_size, 3))
+    image_names, imgs = os.listdir(img_path), []
+    cut_img_cell_center_path = make_dir(base_dir, "cut_img_cell_center")
+
+    # build the model from a config file and a checkpoint file
+    cfg = mmcv.Config.fromfile(config)
+    cfg = update_legacy_cfg(cfg)
+    model = init_segmentor(
+        cfg,
+        checkpoint,
+        device=device,
+        revise_checkpoint=[(r'^module\.', ''), ('model.', '')])
+
+    for image_name in image_names:
+        prefix = image_name.split('.')[0]
+        image = os.path.join(img_path, image_name)
+
+        result = inference_segmentor(model, image)
+        pred = result_to_inst(result[0], threshold=threshold)[0]
+        np.save(os.path.join(prediction_result_path, prefix + ".npy"), pred)
+
+        ori_img = cv2.imread(image)
+        overlay = make_outline_overlay(np.expand_dims(ori_img, 0), np.expand_dims(pred, 0))
+        cv2.imwrite(os.path.join(boundary_result_path, image_name + ".PNG"), overlay[0])
+
+        # determine cell center
+        cut_img_cell_center = get_cell_center_and_area(pred, os.path.join(cut_img_cell_center_path, prefix + ".csv"))
+        cut_img_cell_center["row"] += (int(float(prefix.split("_")[0])) - 1) * cut_size
+        cut_img_cell_center["col"] += (int(float(prefix.split("_")[1])) - 1) * cut_size
+        infos = pd.concat([infos, cut_img_cell_center])
+
+        row_n, col_n = int(float(prefix.split("_")[0])) - 1, int(float(prefix.split("_")[1])) - 1
+        boundery_img[cut_size * row_n: cut_size * (row_n + 1), cut_size * col_n: cut_size * (col_n + 1),
+        :] = overlay[0]
+
+    infos["cell_index"] = [int(i) for i in range(1, infos.shape[0] + 1)]
+    infos.sort_values("col", inplace=True)
+    infos.to_csv(os.path.join(base_dir, "cell_center.csv"), sep=",", header=True, index=False)
+
+    boundery_img = boundery_img[:row_size - cut_size, :col_size - cut_size, :]
+    cv2.imwrite(os.path.join(base_dir, "boundary.PNG"), boundery_img)
+
+
+def get_cell_center_and_area(pred_result, save_path):
+    areas = []  # save the area of each nucleus
+    centers = np.zeros((pred_result.max(), 2), 'int')  # save the center coordinate of each nucleus
+
+    slices = ndimage.find_objects(pred_result)
+    for i, si in enumerate(slices):
+        if si is not None:
+            sr, sc = si
+            yi, xi = np.nonzero(pred_result[sr, sc] == (i + 1))
+            yi = yi.astype(np.int32)
+            xi = xi.astype(np.int32)
+            ymed = np.median(yi)
+            xmed = np.median(xi)
+            imin = np.argmin((xi - xmed) ** 2 + (yi - ymed) ** 2)
+            xmed = xi[imin]
+            ymed = yi[imin]
+            centers[i, 0] = ymed + sr.start
+            centers[i, 1] = xmed + sc.start
+            areas.append(len(yi))
+    info = pd.DataFrame()
+    info["row"] = centers[:, 0]
+    info["col"] = centers[:, 1]
+    info["area"] = areas
+    info.to_csv(save_path, sep=",", header=True, index=False)
+
+    return info
+
+
+# filter nuleus
+def filter_nucleus(save_path, mode, top_value, bottom_value):
+    cell_info = pd.read_csv(os.path.join(save_path, "cell_center.csv"), sep=",", header=0)
+    if mode == 1:
+        cell_info = cell_info[
+            (cell_info["area"] > (bottom_value ** 2) * 3.14) & (cell_info["area"] < (top_value ** 2) * 3.14)]
+    if mode == 2:
+        cell_info.sort_values(by="area", inplace=True, ascending=True)
+        cell_num = cell_info.shape[0]
+        cell_info = cell_info.iloc[int(cell_num / 100 * bottom_value):cell_num - int(cell_num / 100 * top_value)]
+
+    cell_info.to_csv(os.path.join(save_path, "filtered_cell_center.csv"), sep=",", header=True, index=False)
+
+
+def nucleus_recongnition_cut_img(data_path, output_path, device, mode, top_value, bottom_value, threshold):
+    device = "cuda" if device == "GPU" else "cpu"
+    star_time = time.time()
+    all_section = os.listdir(data_path)
+    for item in all_section:
+        dapi_path = find_dapi_img(os.path.join(data_path, item))
+        save_path = os.path.join(output_path, item, "1_nucleus_recongnition_result")
+        os.makedirs(save_path, exist_ok=True)
+
+        # cut images
+        cut_img_result_path = make_dir(save_path, "cut_img_result")
+        cut_img(dapi_path, cut_img_result_path)
+
+        # # Detected nucleus and determine the center of cell
+        prediction_result_path = make_dir(save_path, "nucleus_detect_result")
+        boundary_result_path = make_dir(save_path, "nucleus_boundary_result")
+        predict(save_path, cut_img_result_path, prediction_result_path, boundary_result_path, device, threshold)
+
+        # filter nuleus
+        filter_nucleus(save_path, mode, top_value, bottom_value)
+
+    print(f" detecting nucleus is finished, runtime：{time.time() - star_time}")
---
+++ haha/deep_learing_model/configs/config_cut_img.py
@@ -0,0 +1,196 @@
+log_config = dict(
+    interval=50, hooks=[dict(type='TextLoggerHook', by_epoch=False),
+                        dict(type='TensorboardLoggerHook')])
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+load_from = None
+resume_from = None
+workflow = [('train', 1)]
+cudnn_benchmark = True
+norm_cfg = dict(type='BN', requires_grad=True)
+find_unused_parameters = True
+model = dict(
+    type='EncoderDecoderCellPose',
+    backbone=dict(
+        type='mit_b2',
+        style='pytorch',
+        pretrained='pretrained/mit_b2.pth',
+    ),
+    decode_head=dict(
+        type='CellPoseHead',
+        in_channels=[64, 128, 320, 512],  # b1b2[64, 128, 320, 512]  b0[32, 64, 160, 256]
+        in_index=[0, 1, 2, 3],
+        channels=256,
+        dropout_ratio=0.1,
+        num_classes=3,
+        norm_cfg=norm_cfg,
+        align_corners=False,
+        decoder_params=dict(
+            embed_dims=256,
+            embed_cfg=dict(type='mlp', act_cfg=None, norm_cfg=None),
+            embed_neck_cfg=dict(type='mlp', act_cfg=None, norm_cfg=None),
+            fusion_cfg=dict(
+                type='aspp',
+                sep=True,
+                dilations=(1, 6, 12, 18),
+                pool=False,
+                act_cfg=dict(type='ReLU'),
+                norm_cfg=norm_cfg)),
+
+        # 这里的loss没啥用，只是防止报错
+        loss_decode=dict(
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)
+        # loss_decode=[dict(type='CrossEntropyLoss', use_sigmoid=False, loss_name='loss_ce', loss_weight=1.0),
+        #              dict(type='DiceLoss', loss_name='loss_dice', loss_weight=3.0)]
+    ),
+    train_cfg=dict(
+        work_dir=
+        './work_dirs/gta2cs_uda_warm_fdthings_rcs_croppl_a999_daformer_mitb5_s0'
+    ),
+    # test_cfg=dict(mode='whole')
+    test_cfg=dict(mode='slide2', crop_size=(256, 256), stride=(256, 256), crop_output_size=(200, 200))
+)
+
+source_data_root = 'data/dsb2018/'
+target_data_root = 'data/tissuenet_1.0/'
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+# crop_size = (192, 192)
+source_train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotationsNpy'),
+    # dict(type='Resize', img_scale=(1280, 720)),
+    # dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
+    # dict(type='RandomFlip', prob=0.5),
+    # dict(type='GetHVMap'),
+    dict(type='GetCellPoseMap'),
+    dict(
+        type='Normalize99',
+    ),
+    dict(type='Pad', size_divisor=32, pad_val=0, seg_pad_val=255),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_semantic_seg', 'gt_instance', 'gt_vec'],
+         meta_keys=('filename', 'ori_filename', 'ori_shape',
+                    'img_shape', 'pad_shape', 'scale_factor',
+                    'img_norm_cfg')
+         )
+]
+target_train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotationsNpy'),
+    # dict(type='Resize', img_scale=(1024, 512)),
+    # dict(type='RandomCrop', crop_size=crop_size),
+    # dict(type='RandomFlip', prob=0.5),
+    dict(
+        type='Normalize99',
+    ),
+    dict(type='Pad', size_divisor=32, pad_val=0, seg_pad_val=255),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_semantic_seg'],
+         meta_keys=('filename', 'ori_filename', 'ori_shape',
+                    'img_shape', 'pad_shape', 'scale_factor',
+                    'img_norm_cfg')
+         )
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        keep_original_size=True,
+        img_scale=(256, 256),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(
+                type='Normalize99',
+            ),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img'])
+        ])
+]
+data = dict(
+    samples_per_gpu=4,
+    workers_per_gpu=4,
+    train=dict(
+        type='UDADataset',
+        source=dict(
+            type='NucleiCellPoseDataset',
+            data_root=source_data_root,
+            img_dir='images/train/',
+            ann_dir='labels/train/',
+            pipeline=source_train_pipeline),
+        target=dict(
+            type='NucleiCellPoseDataset',
+            data_root=target_data_root,
+            img_dir='images/test/',
+            ann_dir='labels/test/',
+            pipeline=target_train_pipeline),
+        # rare_class_sampling=dict(
+        #     min_pixels=3000, class_temp=0.01, min_crop_ratio=0.5)
+    ),
+    val=dict(
+        type='NucleiCellPoseDataset',
+        data_root=target_data_root,
+        img_dir='images/test/',
+        ann_dir='labels/test/',
+        pipeline=test_pipeline),
+    test=dict(
+        type='NucleiCellPoseDataset',
+        data_root=target_data_root,
+        img_dir='images/test/',
+        ann_dir='labels/test/',
+        pipeline=test_pipeline))
+uda = dict(
+    type='DAHVBeta',
+    # trade_off=0.0000000001,
+    # alpha=0.999,
+    proto_w=1,
+    stage1=1400000
+    # enable_pseudo_label=False,
+    # # pseudo_threshold=0.85,
+    # debug_img_interval=100,
+    # domaindiscriminator=dict(type='DomainDiscriminator', num_classes=2),
+    # blur=True,
+    # color_jitter_strength=0.2,
+    # color_jitter_probability=0.2,
+    # pseudo_start_iter=3500
+)
+
+use_ddp_wrapper = True
+optimizer = dict(
+    type='AdamW',
+    lr=1e-4,
+    betas=(0.9, 0.999),
+    weight_decay=0.01,
+    paramwise_cfg=dict(
+        custom_keys=dict(
+            # head=dict(lr_mult=10.0),
+            pos_block=dict(decay_mult=0.0),
+            norm=dict(decay_mult=0.0))))
+
+# 这里要设置为None，不需要OptimizerHook，backward在forward_train()
+optimizer_config = None
+# optimizer_config = dict(grad_clip=None)
+lr_config = dict(
+    policy='poly',
+    warmup='linear',
+    warmup_iters=1500,
+    warmup_ratio=1e-06,
+    power=1.0,
+    min_lr=0.0,
+    by_epoch=False)
+seed = 0
+
+runner = dict(type='IterBasedRunner', max_iters=70000)
+checkpoint_config = dict(by_epoch=False, interval=3500)
+evaluation = dict(interval=3500, metric=['mIoU', 'mDice'])
+# name = 'gta2cs_uda_warm_fdthings_rcs_croppl_a999_daformer_mitb5_s0'
+# exp = 'basic'
+# name_dataset = 'gta2cityscapes'
+# name_architecture = 'daformer_sepaspp_mitb5'
+# name_encoder = 'mitb5'
+# name_decoder = 'daformer_sepaspp'
+# name_uda = 'dacs_a999_fd_things_rcs0.01_cpl'
+# name_opt = 'adamw_6e-05_pmTrue_poly10warm_1x2_40k'
+work_dir = './work_dirs/cellpose/dsb2018_mitb0_norm99_bs4/'
